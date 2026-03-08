b7e390fd5894bc63f5e707b771927caff1cc784b
AnshARao <115018391+AnshARao@users.noreply.github.com>
2026-03-08 15:30:39 -0400

    3 intake motors

--- The 2145Z Files/src/subsystems/intake.cpp
+++ The 2145Z Files/src/subsystems/intake.cpp
@@ -5,7 +5,8 @@
 #include "screen.hpp"
 
 pros::Motor intake1(2, pros::v5::MotorGears::blue);
-pros::Motor intake2(1, pros::v5::MotorGears::blue);
+pros::Motor intake2(1, pros::v5::MotorGears::green);
+pros::Motor intake3(3, pros::v5::MotorGears::green);
 
 pros::adi::DigitalOut hood('A', false);
 pros::adi::DigitalOut tunnel('E', false);
@@ -17,6 +18,7 @@ void intake() {
     if (curMatchState != DISABLED) {
         intake1.move(127);
         intake2.move(127);
+        intake3.move(0);
         hood.set_value(false);
         tunnel.set_value(false);
         intakeLift.set_value(false);
@@ -29,10 +31,12 @@ void outtake() {
             intakeLift.set_value(true);
             intake1.move_velocity(-51);
             intake2.move(-127);
+            intake3.move(-127);
         } else {
             intakeLift.set_value(false);
             intake1.move(-127);
             intake2.move(-127);
+            intake3.move(-127);
         }
     }
 }
@@ -42,8 +46,10 @@ void score() {
         intakeLift.set_value(true);
         intake1.move(127);
         intake2.move(127);
+        intake3.move(127);
         hood.set_value(true);
         tunnel.set_value(false);
+        intakeLift.set_value(false);
     }
 }
 
@@ -51,14 +57,16 @@ void scoreMiddle() {
     if (curMatchState != DISABLED) {
         if (midSlow) {
             intake1.move(127);
-            intake2.move_velocity(50);
+            intake2.move(127);
+            intake3.move_velocity(-100);
         } else {
             intake1.move(127);
             intake2.move(127);
+            intake3.move(-127);
         }
-        intakeLift.set_value(true);
         hood.set_value(false);
         tunnel.set_value(true);
+        intakeLift.set_value(false);
     }
 }
 
@@ -66,6 +74,7 @@ void stop() {
     if (curMatchState != DISABLED) {
         intake1.move(0);
         intake2.move(0);
+        intake3.move(0);
     }
 }
 
@@ -80,7 +89,6 @@ void intakeControl() {
         scoreMiddle();
     } else if (controlla.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
         midSlow = !midSlow;
-        //controlla.rumble(".");
     } else {
         stop();
     }
